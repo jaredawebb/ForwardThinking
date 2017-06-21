41a666edb87706d7727c27aa56e1c84d3a6503ab
Jared Webb <webb@math.byu.edu>
2017-06-21 16:14:41 -0600

    bugs

--- replication/gibbs_cifar.py
+++ replication/gibbs_cifar.py
@@ -29,8 +29,8 @@ img_rows, img_cols = 32, 32
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_sacols)
+    x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_sacols)
     input_shape = (1, img_rows, img_cols)
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
