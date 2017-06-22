8af0aa4c54a2c02aa317eb13d9ba3cbb6e1cddcc
Jared Webb <webb@math.byu.edu>
2017-06-21 21:16:57 -0600

    bugs

--- replication/gibbs_cifar.py
+++ replication/gibbs_cifar.py
@@ -193,17 +193,17 @@ for cutoff in cutoffs:
             #train_step.run(feed_dict={x: batch[0], y_: batch[1],
             #                          keep_prob1:0.5, keep_prob2:0.5, keep_prob3:0.5})
             if i < cutoff*epoch_iter:
-                train_steps[i % len(train_steps)].run(feed_dict={x: batch[0],
-                                                                            y_: batch[1],
-                                                                            keep_prob1:0.5,
-                                                                            keep_prob2:0.5,
-                                                                            keep_prob3:0.5})
-                '''
-            #    train_steps[epoch_number % len(train_steps)].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
+            #    train_steps[i % len(train_steps)].run(feed_dict={x: batch[0],
             #                                                                y_: batch[1],
             #                                                                keep_prob1:0.5,
-            #                                                                keep_prob2:0.5})
-            #    '''
+            #                                                                keep_prob2:0.5,
+            #                                                                keep_prob3:0.5})
+                
+                train_steps[epoch_number % len(train_steps)].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
+                                                                            y_: batch[1],
+                                                                            keep_prob1:0.5,
+                                                                            keep_prob2:0.5})
+
             else:
                 if epoch_iter*cutoff == i:
                     print("Switching to output layer only.")
