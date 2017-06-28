57a50351a2e4c14a8a78d2ed5042f22018a5e5ed
Jared Webb <webb@math.byu.edu>
2017-06-28 15:32:54 -0600

    bugs

--- replication/forward_conv_mnist_augmentation.py
+++ replication/forward_conv_mnist_augmentation.py
@@ -352,7 +352,7 @@ with tf.variable_scope("3output"):
 print(y_conv.get_shape())
     
 learning_rate = tf.placeholder(tf.float32, shape=[])
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv_drop, labels=y_))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
