f4e410f28a06b378c1a90ae255ac2f5f90254cd4
Jared Webb <webb@math.byu.edu>
2017-07-03 09:43:17 -0600

    Added big experiment

--- replication/mnist_gibbs_experiments.py
+++ replication/mnist_gibbs_experiments.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import sys
+import time
 
 import keras
 from keras import backend as K
@@ -43,6 +44,8 @@ def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')
 
+technique = 1
+
 batch_size = 128
 num_classes = 10
 img_rows, img_cols = 28, 28
@@ -78,7 +81,7 @@ datagen = ImageDataGenerator(
 datagen.fit(x_train)
 images = datagen.flow(x_train, y_train, batch_size=batch_size)
 
-epochs = 150
+epochs = 100
 epoch_iter = len(x_train) // batch_size
 total_iter = epoch_iter*epochs
 
@@ -86,73 +89,88 @@ x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 x_image = tf.reshape(x, [-1,28,28,1])
-with tf.variable_scope("layer1"):
-    h_conv1 = conv_relu(x_image, [3, 3, 1, 256], [256])
-    h_pool1 = max_pool_2x2(h_conv1)
-#######
 
-with tf.variable_scope("layer2"):
-    h_conv2 = conv_relu(h_pool1, [3, 3, 256, 256], [256])
-    h_pool2 = max_pool_2x2(h_conv2)
+architectures = [[64, 64], [128, 64], [128, 128], [256, 128], [256, 256], [512, 256], [512, 512],
+                 [64, 64, 32], [128, 64, 64], [128, 128, 64], [256, 128, 128], [256, 256, 128],
+                 [512, 256, 256], [512, 512, 256]]
 
-#######
-with tf.variable_scope("layer3"):
-    h_conv3 = conv_relu(h_pool2, [3, 3, 256, 128], [128])
-    h_pool3 = max_pool_2x2(h_conv3)
 
-#######
+for arch in architectures:
 
-flat_dim = int(h_pool3.get_shape()[1]*h_pool3.get_shape()[2]*h_pool3.get_shape()[3])
-
-with tf.variable_scope("fullyconnected"):
-    h_pool3_flat = tf.reshape(h_pool3, [-1, flat_dim])
+    # Get start time
+    t1 = time.time()
     
-    keep_prob1 = tf.placeholder(tf.float32, shape=[])
-    h_pool3_drop = tf.nn.dropout(h_pool3_flat, keep_prob1)
+    # Set up architecture
+    with tf.variable_scope("layer1"):
+        h_conv1 = conv_relu(x_image, [3, 3, 1, arch[0]], [arch[0]])
+        h_pool1 = max_pool_2x2(h_conv1)
+
+
+    with tf.variable_scope("layer2"):
+        h_conv2 = conv_relu(h_pool1, [3, 3, arch[0], arch[1]], arch[1])
+        h_pool2 = max_pool_2x2(h_conv2)
+        
+    if len(arch) == 2:
+        layers = ['layer1', 'layer2', 'fullyconnected', 'output']
+        flat_dim = int(h_pool2.get_shape()[1]*h_pool2.get_shape()[2]*h_pool2.get_shape()[3])
+
+        with tf.variable_scope("fullyconnected"):
+            h_pool2_flat = tf.reshape(h_pool2, [-1, flat_dim])
+
+            keep_prob1 = tf.placeholder(tf.float32, shape=[])
+            h_pool2_drop = tf.nn.dropout(h_pool2_flat, keep_prob1)
+
+            h_fc1 = full_relu(h_pool2_drop, [flat_dim, 150])
+
+            keep_prob2 = tf.placeholder(tf.float32, shape=[])
+            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob2)
+    else:
+        layers = ['layer1', 'layer2', 'layer3', 'fullyconnected', 'output']
+        with tf.variable_scope("layer3"):
+            h_conv3 = conv_relu(h_pool2, [3, 3, arch[1], arch[2]], arch[2])
+            h_pool3 = max_pool_2x2(h_conv3)
+        
+        flat_dim = int(h_pool3.get_shape()[1]*h_pool3.get_shape()[2]*h_pool3.get_shape()[3])
+
+        with tf.variable_scope("fullyconnected"):
+            h_pool3_flat = tf.reshape(h_pool3, [-1, flat_dim])
+
+            keep_prob1 = tf.placeholder(tf.float32, shape=[])
+            h_pool3_drop = tf.nn.dropout(h_pool3_flat, keep_prob1)
+
+            h_fc1 = full_relu(h_pool3_drop, [flat_dim, 150])
+
+            keep_prob2 = tf.placeholder(tf.float32, shape=[])
+            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob2)
+
+    with tf.variable_scope("output"):
+        y_conv = full_relu(h_fc1_drop, [150, 10])
+
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
+
+    # Get trainable variables from each layer
+    train_vars = [tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, layer) for layer in layers]
+
+    # Set up rate decay
+    global_step = tf.Variable(0, trainable=False)
+    starter_learning_rate = 0.005
+    base = 0.98
+    decay_step = decay_steps(base, total_iter)
+    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
+                                               decay_step, base, staircase=False)
     
-    h_fc1 = full_relu(h_pool3_drop, [flat_dim, 150])
+    optimizer = tf.train.AdamOptimizer(learning_rate)
 
-    keep_prob2 = tf.placeholder(tf.float32, shape=[])
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob2)
+    train_steps = [optimizer.minimize(cross_entropy,
+                                      var_list=train_vars[i] + train_vars[-2] + train_vars[-1],
+                                      global_step=global_step) for i in range(len(layers)-2)]
     
-
-with tf.variable_scope("output"):
-    y_conv = full_relu(h_fc1_drop, [150, 10])
-
-
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
-
-layers = ['layer1', 'layer2', 'layer3', 'fullyconnected', 'output']
-train_vars = [tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, layer) for layer in layers]
-
-global_step = tf.Variable(0, trainable=False)
-starter_learning_rate = 0.001
-base = 0.98
-decay_step = decay_steps(base, total_iter)
-learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
-                                           decay_step, base, staircase=False)
-
-optimizer = tf.train.AdamOptimizer(learning_rate)
-
-train_steps = [optimizer.minimize(cross_entropy,
-                                  var_list=train_vars[i] + train_vars[-2] + train_vars[-1],
-                                  global_step=global_step) for i in range(len(layers)-2)]
-
-train_step = optimizer.minimize(cross_entropy, global_step=global_step)
-train_step_new = optimizer.minimize(cross_entropy,
-                                    var_list=train_vars[-3] + train_vars[-2] + train_vars[-1],
-                                    global_step=global_step)
-
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-init_op = tf.global_variables_initializer()
-
-cutoffs = [100, 150]
-learning_rates = [0.005, 0.002, 0.001, 0.0005, 0.0001, 0.00005]
-
-lr = learning_rates[0]
-for cutoff in cutoffs:
+    global_train_step = optimizer.minimize(cross_entropy, global_step=global_step)
+    
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    init_op = tf.global_variables_initializer()
+    
     with tf.Session() as sess:
         sess.run(init_op)
         accuracies = []    
@@ -170,9 +188,10 @@ for cutoff in cutoffs:
                 train_accuracy = accuracy.eval(feed_dict={x:batch[0].reshape((len(batch[0]), 784)), 
                                                           y_: batch[1],
                                                           keep_prob1: 1., 
-                                                          keep_prob2: 1.,
-                                                          learning_rate: lr})
+                                                          keep_prob2: 1.})
 
+                
+                '''
                 acc1 = accuracy.eval(feed_dict={x: x_test[:1000].reshape((1000, 784)), y_: y_test[:1000], keep_prob1:1., keep_prob2:1., learning_rate: lr})
                 acc2 = accuracy.eval(feed_dict={x: x_test[1000:2000].reshape((1000, 784)), y_: y_test[1000:2000], keep_prob1:1., keep_prob2:1., learning_rate: lr})
                 acc3 = accuracy.eval(feed_dict={x: x_test[2000:3000].reshape((1000, 784)), y_: y_test[2000:3000], keep_prob1:1., keep_prob2:1., learning_rate: lr})
@@ -185,65 +204,65 @@ for cutoff in cutoffs:
                 acc10 = accuracy.eval(feed_dict={x: x_test[9000:].reshape((1000, 784)), y_: y_test[9000:], keep_prob1:1., keep_prob2:1., learning_rate: lr})
 
                 acc = np.mean([acc1, acc2, acc3, acc4, acc5, acc6, acc7, acc8, acc9, acc10])
+                '''
+                acc = accuracy.eval(feed_dict={x: x_test.reshape(10000, 784), y_:y_test, keep_prob1:1., keep_prob2:2})
+                curr_time = time.time()
+                accuracies.append((acc, curr_time - t1))
 
-                accuracies.append(acc)
-
-                print("step %d, training accuracy %g, testing accuracy %g, learning rate %g" %(i, train_accuracy, acc, learning_rate))
-
-            if i < cutoff*epoch_iter:
+                print("step %d, training accuracy %g, testing accuracy %g, learning rate %g, time %g" %(i, train_accuracy, acc, learning_rate, curr_time - t1))
                 
-                #train_step.run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
-                #                                                            y_: batch[1],
-                #                                                            keep_prob1:0.3,
-                #                                                            keep_prob2:0.5,
-                #                                                            learning_rate:lr})
+                sys.stdout.flush()
                 
+            if technique == 0:
                 train_steps[i % len(train_steps)].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
+                                                                                y_: batch[1],
+                                                                                keep_prob1:0.3,
+                                                                                keep_prob2:0.5})
+            elif technique == 1:
+                global_train_step.run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
                                                                             y_: batch[1],
                                                                             keep_prob1:0.3,
-                                                                            keep_prob2:0.5,
-                                                                            learning_rate: lr})
+                                                                            keep_prob2:0.5})
                 
-                #train_steps[epoch_number % len(train_steps)].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
-                #                                                            y_: batch[1],
-                #                                                            keep_prob1:0.3,
-                #                                                            keep_prob2:0.5,
-                #                                                            learning_rate: lr})
+            elif technique == 2:
+                if epoch_number == 0:
+                    train_steps[0].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
+                                                                                y_: batch[1],
+                                                                                keep_prob1:0.3,
+                                                                                keep_prob2:0.5})
+                elif epoch_number == 1:
+                    train_steps[1].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
+                                                  y_: batch[1],
+                                                  keep_prob1:0.3,
+                                                  keep_prob2:0.5})
+                else:
+                    train_steps[-1].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)),
+                                                  y_: batch[1],
+                                                  keep_prob1:0.3,
+                                                  keep_prob2:0.5})
+    
                 
-            else:
-                if epoch_iter*cutoff == i:
-                    print("Switching to output layer only.")
-                #train_steps[-1].run(feed_dict={x: batch[0].reshape((len(batch[0]),784)), y_: batch[1],
-                #                      keep_prob1:0.3, keep_prob2:0.5})
-                train_step_new.run(feed_dict={x: batch[0].reshape((len(batch[0]),784)), y_: batch[1],
-                                      keep_prob1:0.3, keep_prob2:0.5, learning_rate: lr})
-                
-            if i == epoch_iter*5:
-                lr = learning_rates[1]
-                print("Learning Rate Updated to: " + str(lr))
-                #train_step = tf.train.AdamOptimizer(learning_rates[1]).minimize(cross_entropy)
-                #sess.run(tf.global_variables_initializer())
-
-            elif i == epoch_iter*10:
-                lr = learning_rates[2]
-                print("Learning Rate Updated to: " + str(lr))
-                #train_step = tf.train.AdamOptimizer(learning_rates[2]).minimize(cross_entropy)
-                #sess.run(tf.global_variables_initializer())
-
-            elif i == epoch_iter*40:
-                lr = learning_rates[3]
-                print("Learning Rate Updated to: " + str(lr))
-                #train_step = tf.train.AdamOptimizer(learning_rates[3]).minimize(cross_entropy)
-                #sess.run(tf.global_variables_initializer())
-
-            elif i == epoch_iter*60:
-                lr = learning_rates[4]
-                print("Learning Rate Updated to: " + str(lr))
-                #train_step = tf.train.AdamOptimizer(learning_rates[4]).minimize(cross_entropy)
-                #sess.run(tf.global_variables_initializer())
-
-            elif i == epoch_iter*80:
-                lr = learning_rates[5]
-                print("Learning Rate Updated to: " + str(lr))
-            sys.stdout.flush()
+        title_string = 'accuracies'
+        for size in arch:
+            title_string += '_' + str(size)
         np.save('accuracies_'+str(cutoff), accuracies)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
